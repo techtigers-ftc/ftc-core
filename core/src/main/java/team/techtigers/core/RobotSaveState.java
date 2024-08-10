5abf74f61b8e0efd0eaa010ca7069e17f9e365ba
Thornblaster12345 <alexvinuk@icloud.com>
2024-08-10 20:09:37 +0400

    Indentation and minor fixes

--- core/src/main/java/team/techtigers/core/RobotSaveState.java
+++ core/src/main/java/team/techtigers/core/RobotSaveState.java
@@ -9,13 +9,14 @@ public class RobotSaveState {
     private HashMap<String, Object> state;
     private static RobotSaveState instance;
 
-    private RobotSaveState(){
+    private RobotSaveState() {
         state = new HashMap<>();
     }
 
     /**
      * If there is an instance of the class, returns it
      * Otherwise, creates a new instance and returns it
+     *
      * @return The instance of the class
      */
     public static RobotSaveState getInstance() {
@@ -28,23 +29,26 @@ public class RobotSaveState {
 
     /**
      * Gets the current robot state
+     *
      * @return The current robot state
      */
     public Object getState(String key) {
         if (!state.containsKey(key)) {
             throw new IllegalStateException("No state has been set for key: " + key);
-        }
+        } else {
             return state.get(key);
+        }
     }
 
     /**
      * Sets the current robot state, requires a string for identification
      * and object for the value to be saved
-     * @param key The key to be used for identification
+     *
+     * @param key   The key to be used for identification
      * @param value The value to be saved
      */
     public void setState(String key, Object value) {
-        if (key == null){
+        if (key == null) {
             throw new NullPointerException("Key cannot be null");
         } else {
             state.put(key, value);
